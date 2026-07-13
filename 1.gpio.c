0595ddc04e81f6c82099abd96efe6c999859c240
yashu <yasaswiv07@gmail.com>
2026-07-13 19:25:19 +0530

    adding gpio.c file

---
+++ 1.gpio.c
@@ -0,0 +1,17 @@
+/*******************************************************
+* Name :Vaddadi  Yasaswi
+* KMID : KM64ANF03
+* Date : 13-07-26
+* Program :
+* Sample Input :
+* Sample Output :
+*
+**************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+		return 0;
+}
+
